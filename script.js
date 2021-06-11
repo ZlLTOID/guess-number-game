894b00ca287a3dc78fda67a5428cf8f82cd8a9fd
jan_dolezal <dolezal29@outlook.com>
2021-06-11 22:54:40 +0200

    refactor

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 'use strict';
 const check = document.querySelector('.check');
 const again = document.querySelector('.again');
-const generateRandomNumber = function () {
-    return Math.floor(Math.random(19) * 20) + 1;
-}
+const message = (message) => document.querySelector('.message').textContent = message;
+const numberTableBackground = (numberTableBackground) => document.querySelector('.number-background').style.width = numberTableBackground;
+const isCheckDisabled = (isDisabled) => document.querySelector('.check').disabled = isDisabled ;
+const generateRandomNumber = () => Math.floor(Math.random(19) * 20) + 1;
+const scoreElement = (score) => document.querySelector('.score').textContent = score;
 
-let numberTableBackground = document.querySelector('.number-background');
 let body = document.querySelector('body');
-let guess = parseInt(document.querySelector('.guess').value);
 let highscore = 0;
 let score = 20;
 let number = generateRandomNumber();
 
 check.addEventListener('click', function () {
-    let message = document.querySelector('.message');
-    // let body = document.querySelector('body');
-    let guess = parseInt(document.querySelector('.guess').value);
+    let guess = Number(document.querySelector('.guess').value);
     let numberTable = document.querySelector('.number');
+
     if (!guess) {
-        message.textContent = '⚠️   No number';
+        message('⚠️   No number');
     }
-    if (!isNaN(guess)) {
+    if (guess !== 0) {
         --score;
-        document.querySelector('.score').textContent = score;
+        scoreElement(score);
         if (guess > number) {
-            message.textContent = 'Try lower number';
+            message('Try lower number');
             numberTable.textContent = '↓';
             numberTable.classList.add('down');
             setTimeout(function () {
@@ -32,53 +31,52 @@ check.addEventListener('click', function () {
             }, 101);
         }
         if (guess < number) {
-            message.textContent = 'Try higher number';
-
-            document.querySelector('.number').textContent = '↑';
-            // numberTable.textContent = '↑';
+            message('Try higher number');
+            numberTable.textContent = '↑';
             numberTable.classList.add('up');
+
             setTimeout(function () {
                 numberTable.classList.remove('up');
             }, 101);
         }
+
         if (guess === number) {
-            message.textContent = 'Yeah, that\'s a right number :)';
-            check.disabled = true;
             body.style.backgroundColor = '#32A956';
+
+            message('Yeah, that\'s a right number :)');
+            isCheckDisabled(true);
+            numberTableBackground('30rem');
             numberTable.textContent = number;
-            numberTableBackground.style.width = '30rem';
+
             if (score > highscore) {
                 highscore = score;
                 document.querySelector('.highscore').textContent = score;
             }
         }
         if (score < 1) {
-            message.textContent = 'GAME OVER :E';
-            check.disabled = true;
             body.style.backgroundColor = '#aa0e0e';
+            message('GAME OVER :E');
+            isCheckDisabled(true);
             numberTable.textContent = ':(';
             numberTable.style.width = '12rem';
             numberTable.style.left = '15%';
         }
     }
-    (numberTableBackground, body, guess);
+    (body);
 });
 
 again.addEventListener('click', function () {
-    let guess = document.querySelector('.guess');
-    let message = document.querySelector('.message');
-    let numberTableBackground = document.querySelector('.number-background');
     let numberTable = document.querySelector('.number');
-    generateRandomNumber()
-
-    document.querySelector('.check').disabled = false;
-    guess.value = '';
-    message.textContent = 'Start guessing...';
-    score = 20;
-    document.querySelector('.score').textContent = '20';
+    let guess = document.querySelector('.guess');
+    guess.value = 0;
     numberTable.textContent = '?';
-    numberTableBackground.style.width = '15rem';
     body.style.backgroundColor = '#222';
+    score = 20;
+    scoreElement(score);
+    isCheckDisabled(false);
+    message('Start guessing...');
+    numberTableBackground('15rem');
+    generateRandomNumber() // tohle možná nebude fungovat jinak asi všechno ok
     number = generateRandomNumber();
     (body);
 })
